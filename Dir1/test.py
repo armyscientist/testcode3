197a11f18d0fbf1c0ddf688696f02cf59d1f5c70
armyscientist <85679483+armyscientist@users.noreply.github.com>
2024-03-26 15:03:45 +0530

    Update test.py

--- Dir1/test.py
+++ Dir1/test.py
@@ -3,11 +3,11 @@ import logging
 import mylib
 logger = logging.getLogger(__name__)
 
-def main():
+def main_function():
     logging.basicConfig(filename='myapp.log', level=logging.INFO)
     logger.info('Started')
     mylib.do_something()
     logger.info('Finished')
 
 if __name__ == '__main__':
-    main()
+    main_function()
